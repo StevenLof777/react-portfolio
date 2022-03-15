d4f3914d482fcd6062f1ce370b10d22ba9718617
Jake Finn <lof.steven@gmail.com>
2022-03-15 15:48:57 -0500

    Tweak styles for skills section

--- client/src/components/Skills.js
+++ client/src/components/Skills.js
@@ -1,17 +1,50 @@
 import React from 'react';
+import {Col, Row} from 'react-bootstrap';
+
 
 export default function Skills() {
   return (
     <div className="skills-section">
       <div className="container">
         <h3 className="my-skills">My Skills</h3>
+        {/* <Row>
+          <Col></Col>
+          <Col>
+            <ul className='skill-ul'>
+              <li className=' lang-icons'>
+              <i className="devicon-html5-plain colored">HTML</i>
+              </li>
+              <li>
+              <i className="devicon-css3-plain colored lang-icons">CSS</i>
+              </li>
+              <li>
+              <i className="devicon-react-plain colored lang-icons">React</i> 
+              </li>
+            </ul>
+          </Col>
+          <Col>
+            <ul className='skill-ul'>
+              <li>
+              <i className="devicon-html5-plain colored lang-icons"></i> HTML
+              </li>
+              <li>
+              <i className="devicon-css3-plain colored lang-icons"></i> CSS
+              </li>
+              <li>
+              <i className="devicon-react-plain colored lang-icons"></i> React
+              </li>
+            </ul>
+          </Col>
+          <Col></Col>
+        </Row> */}
+
         <div className="row lang-icons">
-          <div className="col"><i className="devicon-html5-plain-wordmark colored"></i></div>
-          <div className="col"><i className="devicon-css3-plain-wordmark colored"></i></div>
-          <div className="col"><i className="devicon-mysql-plain-wordmark colored"></i></div>
-          <div className="col"><i className="devicon-mongodb-plain-wordmark colored"></i></div>
-          <div className="col"><i className="devicon-react-original-wordmark colored"></i></div>
-          <div className="col"><i className="devicon-javascript-plain colored"></i></div>
+          <div className="col"><i className="devicon-html5-plain bruh-i colored"></i> HTML</div>
+          <div className="col"><i className="devicon-css3-plain bruh-i colored"></i> CSS</div>
+          <div className="col"><i className="devicon-mysql-plain bruh-i colored"></i> MySQL</div>
+          <div className="col"><i className="devicon-mongodb-plain bruh-i colored"></i> Mongodb</div>
+          <div className="col"><i className="devicon-react-original bruh-i colored"></i> React</div>
+          <div className="col"><i className="devicon-javascript-plain bruh-i colored"></i> JavaScript</div>
         </div>
       </div>
     </div>
