cd6400cb78887e9b23340054781c6794aefd6a26
Steven Lofquist <85033776+StevenLof777@users.noreply.github.com>
2022-08-08 14:28:20 -0400

    Delete src directory and make a new one. Add basic css styling

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,9 @@
-// Why can't I do an object destructure
-// import {Nav, Hero, AboutMe, Skills, Projects, Footer}  from './components';
+import React from 'react'
 
-import React from 'react';
-import Nav from './components/Navigation';
-import Hero  from './components/Hero';
-import AboutMe  from './components/AboutMe';
-import Skills  from './components/Skills';
-import Projects  from './components/Projects';
-import Footer from './components/Footer';
-import './assets/styles.css'
-
-export default function App() {
+const App = () => {
   return (
-    <div>
-      <Nav />
-      <Hero />
-
-      <AboutMe />
-      <Skills />
-      <Projects />
-      <Footer />
-    </div>
-  );
+    <div>App</div>
+  )
 }
+
+export default App
