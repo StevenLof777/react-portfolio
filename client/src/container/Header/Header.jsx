a81a37b0ff3c2b3708447521147872abf2fa3d77
Steven Lofquist <85033776+StevenLof777@users.noreply.github.com>
2022-08-12 10:03:15 -0400

    Style header

--- client/src/container/Header/Header.jsx
+++ client/src/container/Header/Header.jsx
@@ -55,17 +55,16 @@ const Header = () => {
         </motion.img>
 
         <motion.div
-          variants={scaleVariants}
-          whileInView={scaleVariants.whileInView}
-          className="app__header-circles"
-          >
-          {[images.flutter, images.redux, images.sass].map((circle, index) => {
-            <div className='circle-cmp app__flex' key={`circle-${index}`}>
-              <img src={circle} alt="circle" />
-            </div>
-          })}
-
-        </motion.div>
+      variants={scaleVariants}
+      whileInView={scaleVariants.whileInView}
+      className="app__header-circles"
+    >
+      {[images.flutter, images.redux, images.sass].map((circle, index) => (
+        <div className="circle-cmp app__flex" key={`circle-${index}`}>
+          <img src={circle} alt="profile_bg" />
+        </div>
+      ))}
+    </motion.div>
 
       </motion.div>
 
