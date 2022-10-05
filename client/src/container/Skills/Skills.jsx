701b207636170151cb38d0d00616b606a5b8b0f3
Steven <lof.steven@gmail.com>
2022-10-05 16:15:14 -0400

    Style skill badges

--- client/src/container/Skills/Skills.jsx
+++ client/src/container/Skills/Skills.jsx
@@ -1,10 +1,10 @@
 import React, {useState, useEffect} from 'react';
-import {AiFillEye, AiFillGithub} from 'react-icons/ai';
 import { motion } from 'framer-motion';
 import ReactToolTip from 'react-tooltip';
 import {AppWrap} from '../../wrapper';
-import { urlFor, client} from '../../client'
+// import { urlFor, client} from '../../client'
 import "./Skills.scss";
+
 import reactIcon from "../../assets/react.png"
 import sass from "../../assets/sass.png"
 import node from "../../assets/node.png"
@@ -57,33 +57,26 @@ const Skills = () => {
 
   return (
     <>
-      <h2 classsName="head-text">Skills</h2>
+      <h2 className="head-text">My Skills</h2>
+      
       <div className='app__skills-container'>
-        <motion.div className='app__skills-list'>
-          {skill.map((skill)=>
-            (
-          <motion.div
-            whileInView={{opacity:[0,1]}}
-            transition={{duration:0.5}}
-            className="app__skills-item app__flex"
-            key={skill.name}
-          >
-            {/* <div className='app__flex' style={{background: skill.bgColor}}>
-              <img src={urlFor(skill.icon)} alt={skill.name} />
-            </div>
-            <p className='p-text '>{skill.name}</p> */}
-            
-              <>
-                <div className='app__flex' >
-                <img src={skill.imageUrl} alt="Skill" />
+        <motion.div className="app__skills-list">
+            {skill.map((skill) => (
+              <motion.div
+                whileInView={{ opacity: [0, 1] }}
+                transition={{ duration: 0.5 }}
+                className="app__skills-item app__flex"
+                key={skill.name}
+              >
+                <div
+                  className="app__flex">
+                  <img src={skill.imageUrl} alt={skill.name} />
                 </div>
-                <p className='p-text '>{skill.name}</p>
-              </>
-
-          </motion.div>
+                <p className="p-text">{skill.name}</p>
+              </motion.div>
             ))}
-        </motion.div>
-      </div>
+          </motion.div>
+        </div>
     </>
   )
 }
