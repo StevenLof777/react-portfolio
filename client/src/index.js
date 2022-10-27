fadccacda7e813681d203bb4aa6ecb80b14c9597
Steven <lof.steven@gmail.com>
2022-10-27 14:42:15 -0400

    Switch to React 18 root API

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,8 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
 import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App/>, document.getElementById("root"))
+import { createRoot } from 'react-dom/client';
+const container = document.getElementById('app');
+const root = createRoot(container);
+
+root.render(<App/>, document.getElementById("root"))
