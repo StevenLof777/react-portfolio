abf09d479c5c3b53ed37db01af6ca822ee393e5b
Steven <lof.steven@gmail.com>
2022-11-26 01:33:32 -0500

    Update about me section.

--- client/src/pages/Home/container/About/About.jsx
+++ client/src/pages/Home/container/About/About.jsx
@@ -19,9 +19,8 @@ const About = () => {
         whileInView={{x: [0, 0], opacity: [0,1]}}
         transition={{duration: 0.5}}
       >
-        Full Stack Developer with background in CNC programming. Recognised as a proactive, positive, and
-        persistent problem solver. Team player with a great attitude. Graduate of Full Stack Coding Boot Camp through
-        Georgia Tech.
+        Full stack developer and former CNC programmer with experience in coding and making ideas come to life. 
+        Fascinated by this ever-evolving sector and creating projects that people anywhere may use.
       </motion.div>
     </>
   )
